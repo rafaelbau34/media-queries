36a6a4241f1e7d4d7c18fe2614c7a5791dad00f4
rafaelbau34 <146866375+rafaelbau34@users.noreply.github.com>
2024-09-18 18:50:53 -0600

    Add files via upload

---
+++ boton/main.js
@@ -0,0 +1,35 @@
+//creación de función flecha
+
+const saludar = () => {
+  alert("Hola, bienvenido a este cuatrimestre");
+};
+
+//creacion de alerta moderna
+const saludarNuevo = () => {
+  Swal.fire({
+    title: "Error!",
+    text: "Do you want to continue",
+    icon: "error",
+    confirmButtonText: "Cool",
+  });
+};
+
+//otro
+const button = () => {
+  Swal.fire({
+    title: "Do you want to save the changes?",
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: "Save",
+    denyButtonText: `Don't save`,
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      Swal.fire("Saved!", "", "success");
+    } else if (result.isDenied) {
+      Swal.fire("Changes are not saved", "", "info");
+    }
+  });
+};
+
+//llamado de la función
